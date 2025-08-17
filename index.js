c6ed7f7a401a34306758a514d9a8f8effd5dfbf6
홍서현 <ghdtjgus76@naver.com>
2025-08-17 21:45:58 +0900

    chore: 중첩 함수로 수정

--- index.js
+++ index.js
@@ -8,47 +8,47 @@ function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playId];
-    let thisAmount = amountFor(perf, play);
+  function amountFor(performance, play) {
+    let result = 0;
 
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (performance.audience > 30) {
+          result += 1000 * (performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (performance.audience > 20) {
+          result += 10000 + 500 * (performance.audience - 20);
+        }
+        result += 300 * performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    return result;
+  }
+
+  for (let performance of invoice.performances) {
+    const play = plays[performance.playId];
+    let thisAmount = amountFor(performance, play);
+
+    volumeCredits += Math.max(performance.audience - 30, 0);
 
     if (play.type === "comedy") {
-      volumeCredits += Math.floor(perf.audience / 5);
+      volumeCredits += Math.floor(performance.audience / 5);
     }
 
     result += ` ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
+      performance.audience
     }석)\n`;
     totalAmount += thisAmount;
   }
 
   result += `총액 ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
-  return result;
-}
-
-function amountFor(performance, play) {
-  let result = 0;
-
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (performance.audience > 30) {
-        result += 1000 * (performance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (performance.audience > 20) {
-        result += 10000 + 500 * (performance.audience - 20);
-      }
-      result += 300 * performance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
-  }
-
   return result;
 }
