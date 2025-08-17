50cd2544a9a0770292c59d2d442ae4df881d06c3
홍서현 <ghdtjgus76@naver.com>
2025-08-17 22:47:11 +0900

    feat: 다형성을 위한 밑작업

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+class PerformanceCalculator {
+  constructor(performance, play) {
+    this.performance = performance;
+    this.play = play;
+  }
+
+  get amount() {
+    let result = 0;
+
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if (this.play.type === "comedy") {
+      result += Math.floor(this.performance.audience / 5);
+    }
+
+    return result;
+  }
+}
+
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice));
 
@@ -11,40 +52,19 @@ function statement(invoice, plays) {
     return result;
 
     function enrichPerformance(performance) {
+      const calculator = new PerformanceCalculator(
+        performance,
+        playFor(performance)
+      );
       const result = Object.assign({}, performance);
       result.play = playFor(result);
-      result.amount = amountFor(result);
-      result.volumeCredits = volumeCreditsFor(result);
+      result.amount = calculator.amount;
+      result.volumeCredits = calculator.volumeCredits;
       return result;
     }
 
     function playFor(performance) {
       return plays[performance.playId];
-    }
-
-    function amountFor(performance) {
-      const play = playFor(performance);
-      let result = 0;
-
-      switch (play.type) {
-        case "tragedy":
-          result = 40000;
-          if (performance.audience > 30) {
-            result += 1000 * (performance.audience - 30);
-          }
-          break;
-        case "comedy":
-          result = 30000;
-          if (performance.audience > 20) {
-            result += 10000 + 500 * (performance.audience - 20);
-          }
-          result += 300 * performance.audience;
-          break;
-        default:
-          throw new Error(`알 수 없는 장르: ${play.type}`);
-      }
-
-      return result;
     }
 
     function volumeCreditsFor(performance) {
