c39ee8d65459de2a7036cb1e67ea0b3f944cb3b6
홍서현 <ghdtjgus76@naver.com>
2025-08-17 22:30:44 +0900

    refactor: 데이터 계층 분리

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  return renderPlainText(createStatementData(invoice));
 
-  return renderPlainText(statementData);
+  function createStatementData(invoice) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+
+    return statementData;
+  }
 
   function playFor(performance) {
     return plays[performance.playId];
